7a6e75c8cafae1d42b6a30843dd4fb4d94fb39f9
sout233 <2273466101@qq.com>
2024-11-06 16:13:29 +0800

    Fix: CPU usage too high bug

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
+use base64::engine::general_purpose;
+use base64::Engine;
 use once_cell::sync::Lazy;
 use salvo::prelude::*;
 use serde::{Deserialize, Serialize};
-use serde_json::json;
+use std::collections::HashMap;
+use std::fmt::Display;
 use std::num::{NonZeroU8, NonZeroUsize};
 use std::path::PathBuf;
+use std::sync::Arc;
 use std::time::Duration;
 use tokio::sync::Mutex;
 
-use anyhow::{Ok, Result};
 use tracing::info;
 use url::Url;
 
@@ -20,7 +23,8 @@ use http_downloader::{
 };
 
 static DOWNLOADER_CREATOR: Lazy<fn() -> HttpDownloaderBuilder> = Lazy::new(|| create_downloader);
-static GLOBAL_DOWNLOAD_TASKS: Lazy<Mutex<Vec<Status>>> = Lazy::new(|| Mutex::new(Vec::new()));
+static GLOBAL_STATUS_LIST: Lazy<Arc<Mutex<HashMap<String, NalaiInfo>>>> =
+    Lazy::new(|| Arc::new(Mutex::new(HashMap::new())));
 
 fn create_downloader() -> HttpDownloaderBuilder {
     let save_dir = PathBuf::from("C:/download");
@@ -30,7 +34,7 @@ fn create_downloader() -> HttpDownloaderBuilder {
     HttpDownloaderBuilder::new(test_url, save_dir)
         .chunk_size(NonZeroUsize::new(1024 * 1024 * 10).unwrap()) // 块大小
         .download_connection_count(NonZeroU8::new(8).unwrap())
-        .file_name(Some("file_name.zip".to_string()))
+    // .file_name(Some("file_name.zip".to_string()))
 }
 
 #[handler]
@@ -48,16 +52,56 @@ async fn start_download() -> String {
         ));
 
     let download_future = downloader.prepare_download().unwrap();
+    let file_path = downloader
+        .get_file_path()
+        .to_str()
+        .unwrap_or_default()
+        .to_string();
+    let id = general_purpose::STANDARD.encode(file_path.as_bytes());
+
 
     // let _status = status_state.status(); // get download status， 获取状态
-    // let _status_receiver = status_state.status_receiver; //status watcher，状态监听器
+    let mut status_receiver = status_state.status_receiver; //status watcher，状态监听器
+
     // let _byte_per_second = speed_state.download_speed(); // get download speed，Byte per second，获取速度，字节每秒
-    let _speed_receiver = speed_state.receiver; // get download speed watcher，速度监听器
+    // let _speed_receiver = speed_state.receiver; // get download speed watcher，速度监听器
+
+    tokio::spawn({
+        let id = id.clone();
+        
+        async move {
+        while status_receiver.changed().await.is_ok() {
+            let status = status_receiver.borrow().clone();
+
+            let mut info = GLOBAL_STATUS_LIST
+                .lock()
+                .await
+                .get(&id)
+                .unwrap_or(&NalaiInfo::default())
+                .clone();
+
+            info.status = format!("{:?}", status.clone());
+
+            GLOBAL_STATUS_LIST
+                .lock()
+                .await
+                .insert(id.clone(), info.clone());
+
+            info!(
+                "Insert status for: {:?}, status: {:?}, file_path: {}",
+                id, info, file_path
+            );
+
+            tokio::time::sleep(Duration::from_millis(1000)).await;
+        }
+    }});
 
     // 打印下载进度
     tokio::spawn({
         let mut downloaded_len_receiver = downloader.downloaded_len_receiver().clone();
         let total_size_future = downloader.total_size_future();
+        let id = id.clone();
+
         async move {
             let total_len = total_size_future.await;
             if let Some(total_len) = total_len {
@@ -76,6 +120,20 @@ async fn start_download() -> String {
                         total_len
                     );
                 }
+
+                let mut info = GLOBAL_STATUS_LIST
+                    .lock()
+                    .await
+                    .get(&id)
+                    .unwrap_or(&NalaiInfo::default())
+                    .clone();
+
+                info.progress = progress;
+
+                GLOBAL_STATUS_LIST
+                    .lock()
+                    .await
+                    .insert(id.clone(), info.clone());
 
                 tokio::time::sleep(Duration::from_millis(1000)).await;
             }
@@ -100,26 +158,30 @@ async fn start_download() -> String {
     result
 }
 
-#[derive(Serialize, Deserialize)]
-struct Status {
-    id: String,
+#[derive(Serialize, Deserialize, Clone, Debug)]
+struct NalaiInfo {
     status: String,
     progress: u64,
     speed: u64,
 }
 
+impl Default for NalaiInfo {
+    fn default() -> Self {
+        Self {
+            status: Default::default(),
+            progress: Default::default(),
+            speed: Default::default(),
+        }
+    }
+}
+
 #[handler]
-async fn get_status(req: &mut Request, res: &mut Response) {
+async fn get_info(req: &mut Request, res: &mut Response) {
     let id = req.query::<String>("id").unwrap_or_default();
 
     info!("Get status for id: {}", id);
 
-    let status = Status {
-        id: id.clone(),
-        status: "Downloading".to_string(),
-        progress: 0,
-        speed: 0,
-    };
+    let status = GLOBAL_STATUS_LIST.lock().await.get(&id).cloned().unwrap();
 
     res.render(Json(status))
 }
@@ -133,7 +195,7 @@ async fn main() {
     tokio::spawn(async {
         let router = Router::new()
             .push(Router::with_path("/download").get(start_download))
-            .push(Router::with_path("/status").get(get_status));
+            .push(Router::with_path("/info").get(get_info));
         let acceptor = TcpListener::new("127.0.0.1:13088").bind().await;
         Server::new(acceptor).serve(router).await;
     });
